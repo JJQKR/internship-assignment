df12ee6e7ebdfdcd7548dc31f2a0788c378088d7
JJQKR <yjcoyjco@naver.com>
2025-01-19 19:47:42 +0900

    [add] sentry

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import * as Sentry from "@sentry/react";
-import { Replay } from "@sentry/replay";
 
 Sentry.init({
-  dsn: "your-dsn-here",
-  integrations: [new Replay()],
+  dsn: "https://88a6d03d2c11a1a92b0bba22c55839b5@o4508669926768640.ingest.us.sentry.io/4508669932732416", // Sentry가 제공한 DSN
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
   tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", "moneyfulpublicpolicy.co.kr"],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
 
 createRoot(document.getElementById("root")!).render(
